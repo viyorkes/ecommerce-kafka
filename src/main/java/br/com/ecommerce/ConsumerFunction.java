73cf115397896db62cce324b1dbf426b095cdd61
victor <victorsleiman25@gmail.com>
2021-07-23 16:28:01 -0300

    Create a consumer Interface

---
+++ src/main/java/br/com/ecommerce/ConsumerFunction.java
@@ -0,0 +1,9 @@
+package br.com.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public interface ConsumerFunction {
+
+
+    void consumer (ConsumerRecord<String, String>record);
+}
